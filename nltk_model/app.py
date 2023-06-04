f6c6d8b62f146ff5c2b107dd70e3c69215d35471
vakhitovalex <vakhitovalex@gmail.com>
2023-06-05 02:38:06 +0300

    feat: add CORS

--- nltk_model/app.py
+++ nltk_model/app.py
@@ -1,8 +1,9 @@
 from compareLetter import predict_letter_quality
 from flask import Flask, request, jsonify
-
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/predict', methods=['POST'])
 
